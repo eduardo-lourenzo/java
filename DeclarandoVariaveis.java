a2b7abe91243c90e9fe460548270c933d2d95832
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2024-12-04 16:45:23 -0300

    feat: add DeclarandoVariaveis class

---
+++ DeclarandoVariaveis.java
@@ -0,0 +1,10 @@
+public class DeclarandoVariaveis {
+    public static void main(String[] args) {
+        String name = "Eduardo";
+        int age = 36;
+        double height = 1.88;
+        boolean isEnrolled = true;
+
+        System.out.println("Nome: " + name + " Idade: " + age + " Altura: " + height + " Matriculado: " + isEnrolled);
+    }
+}
