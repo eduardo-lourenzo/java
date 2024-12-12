d2e321fbb6ae2b55cc000e1efbaa5a58ceea618f
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2024-12-12 10:33:18 -0300

    docs: add requirements

---
+++ desafios/JogoDoAdivinha.java
@@ -0,0 +1,24 @@
+package desafios;
+
+/* 
+ * [ ] [ReqMin01] - Sortear número aleatório dentro de um intervalo.
+ * [ ] [ReqMin02] - Obter palpite do jogador.
+ * [ ] [ReqMin02a] -- Acerto = 10 pontos.
+ * [ ] [ReqMin02b] -- 1 unidade de distãocia = 5 pontos.
+ * [ ] [ReqMin02c] -- Erro = 0 pontos.
+ * [ ] [ReqMin03] - Exibir pontuação ao final da rodada.
+ * [ ] [ReqMed01] - Criar várias rodadas.
+ * [ ] [ReqMed02] - Acumular a pontuação das rodadas.
+ * [ ] [ReqMed03] - Perguntar se continua.
+ * [ ] [ReqMax01] - Adicionar níveis de dificuldade.
+ * [ ] [ReqMax01a] -- Fácil: Números entre 1 e 10.
+ * [ ] [ReqMax01b] -- Médio: Números entre 1 e 50.
+ * [ ] [ReqMax01c] -- Difícil: Números entre 1 e 100.
+ * [ ] [ReqMax02] - Exibir pontuação acumulada das rodadas.
+ * [ ] [ReqMax02a] -- Exibir lista com os acertos.
+ * [ ] [ReqMax02b] -- Exibir lista com os erros.
+ */
+
+public class JogoDoAdivinha {
+    
+}
