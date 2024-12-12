87a5398eb7c870713230dd920862a31ba42918c2
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2024-12-12 11:21:09 -0300

    feat: add constants for difficulty

--- desafios/JogoDoAdivinha.java
+++ desafios/JogoDoAdivinha.java
@@ -3,30 +3,34 @@ package desafios;
 import java.util.Random;
 
 /* 
- * [ ] [ReqMin01] - Sortear número aleatório dentro de um intervalo.
- * [ ] [ReqMin02] - Obter palpite do jogador.
- * [ ] [ReqMin02a] -- Acerto = 10 pontos.
- * [ ] [ReqMin02b] -- 1 unidade de distãocia = 5 pontos.
- * [ ] [ReqMin02c] -- Erro = 0 pontos.
- * [ ] [ReqMin03] - Exibir pontuação ao final da rodada.
- * [ ] [ReqMed01] - Criar várias rodadas.
- * [ ] [ReqMed02] - Acumular a pontuação das rodadas.
- * [ ] [ReqMed03] - Perguntar se continua.
- * [ ] [ReqMax01] - Adicionar níveis de dificuldade.
- * [ ] [ReqMax01a] -- Fácil: Números entre 1 e 10.
- * [ ] [ReqMax01b] -- Médio: Números entre 1 e 50.
- * [ ] [ReqMax01c] -- Difícil: Números entre 1 e 100.
- * [ ] [ReqMax02] - Exibir pontuação acumulada das rodadas.
- * [ ] [ReqMax02a] -- Exibir lista com os acertos.
- * [ ] [ReqMax02b] -- Exibir lista com os erros.
- */
+* [X] [ReqMin01] - Sortear número aleatório dentro de um intervalo.
+* [ ] [ReqMin02] - Obter palpite do jogador.
+* [ ] [ReqMin02a] -- Acerto = 10 pontos.
+* [ ] [ReqMin02b] -- 1 unidade de distãocia = 5 pontos.
+* [ ] [ReqMin02c] -- Erro = 0 pontos.
+* [ ] [ReqMin03] - Exibir pontuação ao final da rodada.
+* [ ] [ReqMed01] - Criar várias rodadas.
+* [ ] [ReqMed02] - Acumular a pontuação das rodadas.
+* [ ] [ReqMed03] - Perguntar se continua.
+* [ ] [ReqMax01] - Adicionar níveis de dificuldade.
+* [ ] [ReqMax01a] -- Fácil: Números entre 1 e 10.
+* [ ] [ReqMax01b] -- Médio: Números entre 1 e 50.
+* [ ] [ReqMax01c] -- Difícil: Números entre 1 e 100.
+* [ ] [ReqMax02] - Exibir pontuação acumulada das rodadas.
+* [ ] [ReqMax02a] -- Exibir lista com os acertos.
+* [ ] [ReqMax02b] -- Exibir lista com os erros.
+*/
 
 public class JogoDoAdivinha {
+    static final int LIMITE_FACIL = 10;
+    static final int LIMITE_MEDIO = 50;
+    static final int LIMITE_DIFICIL = 100;
+
     public static void main(String[] args) {
-        System.out.println(obterNumeroAleatorio(10));
+        System.out.println(obterNumeroAleatorio(LIMITE_FACIL));
     }
 
-    public static int obterNumeroAleatorio(int valorMaximo) {
-        return new Random().nextInt(valorMaximo + 1);
+    public static int obterNumeroAleatorio(int limiteMaximo) {
+        return new Random().nextInt(limiteMaximo + 1);
     }
 }
