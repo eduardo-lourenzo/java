a5d3e97c9c46ad287903495673e56375a3012084
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2024-12-12 14:42:55 -0300

    feat: create multiple rounds

--- desafios/JogoDoAdivinha.java
+++ desafios/JogoDoAdivinha.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.util.Random;
 
 /* 
-* [ ] [ReqMed01] - Criar várias rodadas.
+* [X] [ReqMed01] - Criar várias rodadas.
 * [ ] [ReqMed02] - Acumular a pontuação das rodadas.
 * [ ] [ReqMed03] - Perguntar se continua.
 * [ ] [ReqMax01] - Adicionar níveis de dificuldade.
@@ -22,37 +22,45 @@ public class JogoDoAdivinha {
     static final int LIMITE_DIFICIL = 100;
 
     public static void main(String[] args) {
+        boolean continuarJogando = true;
         int numeroSorteado;
         int numeroDoPalpite;
         int pontuaçãoDaRodada;
         int diferencaAbsoluta;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Sorteando número.");
-        numeroSorteado = obterNumeroAleatorio(LIMITE_FACIL);
+        while (continuarJogando) {
+            System.out.println("Sorteando número.");
+            numeroSorteado = obterNumeroAleatorio(LIMITE_FACIL);
 
-        // [] Capturar excessão se não for um inteiro.
-        System.out.print("Qual o numero do seu palpite? ");
-        numeroDoPalpite = scanner.nextInt();
-
-        while (numeroDoPalpite < 1 || numeroDoPalpite > LIMITE_FACIL) {
-            System.out.println("O palpite está fora da faixa de valores!");
-            System.out.print("Digite um número entre 1 e " + LIMITE_FACIL + ": ");
+            // [] Capturar excessão se não for um inteiro.
+            System.out.print("Qual o numero do seu palpite? ");
             numeroDoPalpite = scanner.nextInt();
+
+            while (numeroDoPalpite < 1 || numeroDoPalpite > LIMITE_FACIL) {
+                System.out.println("O palpite está fora da faixa de valores!");
+                System.out.print("Digite um número entre 1 e " + LIMITE_FACIL + ": ");
+                numeroDoPalpite = scanner.nextInt();
+            }
+
+            diferencaAbsoluta = Math.abs(numeroSorteado - numeroDoPalpite);
+            if (diferencaAbsoluta == 0) {
+                pontuaçãoDaRodada = 10;
+            } else if (diferencaAbsoluta == 1) {
+                pontuaçãoDaRodada = 5;
+            } else {
+                pontuaçãoDaRodada = 0;
+            }
+
+            System.out.println("Pontuação da rodada = " + pontuaçãoDaRodada + ".");
+            System.out.println("Sorteado = " + numeroSorteado + " : " + numeroDoPalpite + " = Palpite");
+
+            // [ ] [fix] - Tratar entrada do usuário.
+            // [ ] [feat] - Cirar função que retorna um booleano.
+            System.out.println("Deseja continuar jogando?");
+            System.out.print("0 = Não | 1 = Sim: ");
+            continuarJogando = (scanner.nextInt() == 0) ? false : true;
         }
-
-        diferencaAbsoluta = Math.abs(numeroSorteado - numeroDoPalpite);
-        if (diferencaAbsoluta == 0) {
-            pontuaçãoDaRodada = 10;
-        }else if (diferencaAbsoluta == 1) {
-            pontuaçãoDaRodada = 5;
-        } else {
-            pontuaçãoDaRodada = 0;
-        }
-
-        System.out.println("Pontuação da rodada = " + pontuaçãoDaRodada + ".");
-        System.out.println("Sorteado = " + numeroSorteado + " : " + numeroDoPalpite + " = Palpite");
-
         scanner.close();
     }
 
