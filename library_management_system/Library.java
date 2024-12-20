930f2b57b33062d5cacb61a6cf92fa27ac809e0e
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2024-12-20 15:00:18 -0300

    feat: add attributes to Library class

--- library_management_system/Library.java
+++ library_management_system/Library.java
@@ -1,4 +1,8 @@
 package library_management_system;
 
+import java.util.ArrayList;
+
 public class Library {
+    ArrayList<Book> listOfBooks = new ArrayList<Book>();
+    ArrayList<User> listOfUsers = new ArrayList<User>();
 }
