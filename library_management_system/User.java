fc54a38fd0cad05bce002147e072d05c8c58b30f
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2024-12-20 15:24:30 -0300

    fix: instantiate BorrowedBooks inside the constructor

--- library_management_system/User.java
+++ library_management_system/User.java
@@ -5,11 +5,12 @@ import java.util.ArrayList;
 public class User {
     private String name;
     private int id;
-    private ArrayList<Book> borrowedBooks = new ArrayList<Book>();
+    private ArrayList<Book> borrowedBooks;
 
     public User(String name, int id) {
         this.name = name;
         this.id = id;
+        this.borrowedBooks  = new ArrayList<Book>();
     }
 
     public String showDetails() {
