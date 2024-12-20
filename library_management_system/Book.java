5939868748209bff2b19511cf6e59d307b80a24b
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2024-12-20 11:37:20 -0300

    feat: add methods to Book class

--- library_management_system/Book.java
+++ library_management_system/Book.java
@@ -5,4 +5,16 @@ public class Book {
     private String author;
     private String isbn;
     private boolean isAvailable;
+
+    public String showDetails() {
+        return "Título: " + title + ", Autor: " + author + ", ISBN: " + isbn;
+    }
+
+    public void borrow() {
+        isAvailable = false;
+    }
+
+    public void devolve() {
+        isAvailable = true;
+    }
 }
