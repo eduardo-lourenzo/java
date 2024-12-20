f7dc38d960d0cbcc6083d8a8327619ad0b477ccd
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2024-12-20 11:45:05 -0300

    feat: add attributes to User class

--- library_management_system/User.java
+++ library_management_system/User.java
@@ -1,4 +1,9 @@
 package library_management_system;
 
+import java.util.ArrayList;
+
 public class User {
+    private String name;
+    private int id;
+    private ArrayList<Book> borrowedBooks;
 }
