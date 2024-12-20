beab4554e66866d153241d079e2ba5db97c6288a
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2024-12-20 16:06:51 -0300

    fix: instantiate listOfBooks and listOfUsers inside the constructor

--- library_management_system/Library.java
+++ library_management_system/Library.java
@@ -3,6 +3,11 @@ package library_management_system;
 import java.util.ArrayList;
 
 public class Library {
-    ArrayList<Book> listOfBooks = new ArrayList<Book>();
-    ArrayList<User> listOfUsers = new ArrayList<User>();
+    ArrayList<Book> listOfBooks;
+    ArrayList<User> listOfUsers;
+
+    public Library() {
+        this.listOfBooks = new ArrayList<Book>();
+        this.listOfUsers = new ArrayList<User>();
+    }
 }
