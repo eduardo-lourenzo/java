72ce4126c0d9cbd674f1b1cc4e2ed694fec1f005
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2024-12-21 20:15:10 -0300

    feat: add attributes in Main class

--- library_management_system/Main.java
+++ library_management_system/Main.java
@@ -2,5 +2,9 @@ package library_management_system;
 
 public class Main {
     public static void main(String[] args) {
+        Library library = new Library();
+        User userTemporary;
+        Book bookTemporary;
+
     }
 }
