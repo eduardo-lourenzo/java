3ccffab47b4a107263484d96aae8aa5ef1b9e4d6
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2024-12-21 20:23:40 -0300

    feat: show menu in Main class

--- library_management_system/Main.java
+++ library_management_system/Main.java
@@ -1,10 +1,29 @@
 package library_management_system;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Library library = new Library();
+        int option;
         User userTemporary;
         Book bookTemporary;
+        Library library = new Library();
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Bem-vindo ao Sistema de Gerenciamento de Biblioteca!");
+
+        do {
+            System.out.println("Escolha uma opção:");
+            System.out.println("1. Cadastrar livro.");
+            System.out.println("2. Cadastrar usuário.");
+            System.out.println("3. Realizar empréstimo.");
+            System.out.println("4. Realizar devolução.");
+            System.out.println("5. Exibir livros disponíveis");
+            System.out.println("6. Sair");
+
+        }while(option != 6);
+
+
+        scanner.close();
     }
 }
