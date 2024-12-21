e814485c7fbf105591be276a295deb951536bef4
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2024-12-21 20:59:08 -0300

    feat: add logic in switch-case in Main method

--- library_management_system/Main.java
+++ library_management_system/Main.java
@@ -5,8 +5,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int option;
+        int idUser;
+        String title;
+        String author;
+        String isbn;
+        String name;
         User userTemporary;
-        Book bookTemporary;
+        Book bookTemporary
         Library library = new Library();
         Scanner scanner = new Scanner(System.in);
 
@@ -21,9 +26,66 @@ public class Main {
             System.out.println("5. Exibir livros disponíveis");
             System.out.println("6. Sair");
 
-        }while(option != 6);
+            option = scanner.nextInt();
 
+            switch (option) {
+                case 1: {
+                    System.out.println("Quais as as informações do livro?");
+                    System.out.print("Digite o título do livro: ");
+                    title = scanner.nextLine();
+                    System.out.print("Digite o autor do livro: ");
+                    author = scanner.nextLine();
+                    System.out.print("Digite o ISBN do livro: ");
+                    isbn = scanner.nextLine();
+
+                    bookTemporary = new Book(title, author, isbn);
+                    library.registerBook(bookTemporary);
+
+                    break;
+                }
+                case 2: {
+                    System.out.println("Quais as informações do usuário?");
+                    System.out.print("Digite o nome do usuário: ");
+                    name = scanner.nextLine();
+                    System.out.print("Digite o ID do usuário: ");
+                    idUser = scanner.nextInt();
+
+                    userTemporary = new User(name, idUser);
+                    library.registerUser(userTemporary);
+
+                    break;
+                }
+                case 3: {
+                    System.out.println("Quais as informações do Emprestimo?");
+                    System.out.print("Digite o ISBN do livro: ");
+                    isbn = scanner.nextLine();
+                    System.out.print("Digite o ID do usuário: ");
+                    idUser = scanner.nextInt();
+
+                    library.realizeBorrow(isbn, idUser);
+
+                    break;
+                }
+                case 4: {
+                    System.out.println("Quais as informações do Emprestimo?");
+                    System.out.print("Digite o ISBN do livro: ");
+                    isbn = scanner.nextLine();
+                    System.out.print("Digite o ID do usuário: ");
+                    idUser = scanner.nextInt();
+
+                    library.realizeDevolution(isbn, idUser);
+
+                    break;
+                }
+                case 5: {
+                    library.showAvailableBooks();
+
+                    break;
+                }
+            }
+        } while (option != 6);
 
         scanner.close();
+        System.out.println("Encerrando o Sistema de Gerenciamento de Biblioteca!");
     }
 }
